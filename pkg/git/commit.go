68c18899d0253b9b6e761c112831592b7b6f8056
Johannes Lohmer <lohmer@univention.de>
2025-01-29 01:28:24 +0100

    feat: Add public GetCommit function

--- pkg/git/commit.go
+++ pkg/git/commit.go
@@ -6,7 +6,6 @@ import (
 )
 
 type Commit struct {
-	sha       GitSha
 	tree      GitSha
 	parent    GitSha
 	author    string
@@ -14,30 +13,37 @@ type Commit struct {
 	message   string
 }
 
-// func GetCommit(base: string, sha GitSha) (Commit, error) {
-//
-// }
+func GetCommit(base string, sha GitSha) (*Commit, error) {
+	object, err := getObject(base, sha)
+	if err != nil {
+		return nil, err
+	}
+	commit, err := parseCommit(object)
+	if err != nil {
+		return nil, err
+	}
+	return commit, nil
+}
 
-func parseCommit(sha GitSha, stringCommit string) (*Commit, error) {
-	treeIndex := strings.Index(stringCommit, "\x00tree ")
-	parentIndex := strings.Index(stringCommit, "\nparent ")
-	authorIndex := strings.Index(stringCommit, "\nauthor ")
-	committerIndex := strings.Index(stringCommit, "\ncommitter ")
-	messageIndex := strings.Index(stringCommit, "\n\n")
+func parseCommit(object string) (*Commit, error) {
+	treeIndex := strings.Index(object, "\x00tree ")
+	parentIndex := strings.Index(object, "\nparent ")
+	authorIndex := strings.Index(object, "\nauthor ")
+	committerIndex := strings.Index(object, "\ncommitter ")
+	messageIndex := strings.Index(object, "\n\n")
 
 	fmt.Printf("treeIndex: %d, parentIndex: %d, authorIndex: %d, committerIndex: %d, messageIndex: %d", treeIndex, parentIndex, authorIndex, committerIndex, messageIndex)
 
 	if treeIndex == -1 || parentIndex == -1 || authorIndex == -1 || committerIndex == -1 || messageIndex == -1 {
-		return nil, fmt.Errorf("failed to split commit string:\n%s", stringCommit)
+		return nil, fmt.Errorf("failed to split commit string:\n%s", object)
 	}
 
 	commit := Commit{
-		sha,
-		GitSha(stringCommit[treeIndex+6 : parentIndex]),
-		GitSha(stringCommit[parentIndex+8 : authorIndex]),
-		stringCommit[authorIndex+8 : committerIndex],
-		stringCommit[committerIndex+11 : messageIndex],
-		stringCommit[messageIndex+2:],
+		GitSha(object[treeIndex+6 : parentIndex]),
+		GitSha(object[parentIndex+8 : authorIndex]),
+		object[authorIndex+8 : committerIndex],
+		object[committerIndex+11 : messageIndex],
+		object[messageIndex+2:],
 	}
 
 	return &commit, nil
