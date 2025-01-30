f4e7ef5d61ff968b33deceb0887eaf99ff9fddaa
Johannes Lohmer <lohmer@univention.de>
2025-01-30 19:00:43 +0100

    refactor: extract commit logic into spearate file to prepare for method refactoring

---
+++ pkg/git/commit.go
@@ -0,0 +1,57 @@
+package git
+
+import (
+	"fmt"
+	"log/slog"
+	"strings"
+)
+
+func (repo *Repo) Commit(sha GitSha) (*Commit, error) {
+	object, err := getObject(repo.base, sha)
+	if err != nil {
+		return nil, err
+	}
+	commit, err := parseCommit(object)
+	if err != nil {
+		return nil, err
+	}
+	return commit, nil
+}
+
+func parseCommit(object string) (*Commit, error) {
+	treeIndex := strings.Index(object, "\x00tree ")
+	parentIndex := strings.Index(object, "\nparent ")
+	authorIndex := strings.Index(object, "\nauthor ")
+	committerIndex := strings.Index(object, "\ncommitter ")
+	messageIndex := strings.Index(object, "\n\n")
+
+	slog.Debug("Commit string parsing indexes", "treeIndex", treeIndex, "parentIndex", parentIndex, "authorIndex", authorIndex, "committerIndex", committerIndex, "messageIndex", messageIndex)
+
+	var parents []string
+	var parent string
+	substring := object[parentIndex:]
+
+	for substring[:8] == "\nparent " {
+		parent = substring[8:48]
+		parents = append(parents, parent)
+		substring = substring[48:]
+	}
+
+	slog.Debug("Commit string parsing results", "parents", parents, "substring", substring)
+
+	if treeIndex == -1 || parentIndex == -1 || authorIndex == -1 || committerIndex == -1 || messageIndex == -1 {
+		slog.Warn("Commit message parsing failed", "commit", object)
+		return nil, fmt.Errorf("failed to split commit string:\n%s", object)
+	}
+
+	commit := Commit{
+		GitSha(object[treeIndex+6 : treeIndex+6+40]),
+		GitSha(parent),
+		object[authorIndex+8 : committerIndex],
+		object[committerIndex+11 : messageIndex],
+		object[messageIndex+2:],
+	}
+
+	slog.Debug("Parsed commit", "commit", commit)
+	return &commit, nil
+}
---
+++ pkg/git/commit_test.go
@@ -0,0 +1,74 @@
+package git
+
+import (
+	"github.com/Y0-L0/recursive-git/testutils"
+	"testing"
+)
+
+func TestParseCommit(t *testing.T) {
+	commit, err := parseCommit(EXAMPLE_COMMIT.object)
+	testutils.Ok(t, err)
+
+	testutils.Equals(t, &EXAMPLE_COMMIT.commit, commit)
+}
+
+func TestGetCommit(t *testing.T) {
+	commit, err := testRepo().Commit(EXAMPLE_COMMIT.sha)
+	testutils.Ok(t, err)
+
+	testutils.Equals(t, &EXAMPLE_COMMIT.commit, commit)
+}
+
+var parentTests = []struct {
+	id     string
+	sha    GitSha
+	parent GitSha
+}{
+	{
+		"Normal Commit",
+		GitSha("22950c7aaaf4b990a1f69388f06a003a1462642d"),
+		GitSha("5463cfb060336eb1c6328e6ac44cf4a68779e365"),
+	},
+	// TODO: implement merge commit handling
+	// {
+	// "Merge Commit 1",
+	//  GitSha("b91435bba4bba776634622252b3793afcb711910"),
+	//   GitSha("22950c7aaaf4b990a1f69388f06a003a1462642d"),
+	// },
+	// {
+	// "Merge Commit 2",
+	//  GitSha("2c6bd14b0015249b232685b50ab69016e74cc775"),
+	//   GitSha("153b856314764c5c4adada76156e2ef659539855"),
+	// },
+}
+
+func TestGetParent(t *testing.T) {
+	for _, tt := range parentTests {
+		t.Run(tt.id, func(t *testing.T) {
+			correctParent(t, tt.sha, tt.parent)
+		})
+	}
+}
+
+func correctParent(t *testing.T, sha GitSha, parent GitSha) {
+	commit, err := testRepo().Commit(sha)
+	testutils.Ok(t, err)
+
+	testutils.Equals(t, parent, commit.parent)
+}
+
+func TestGetPackedInitialCommit(t *testing.T) {
+	t.Skip("Not yet implemented")
+	expected := Commit{
+		GitSha("b84acc25f4463b7cdaae512efdac761eac4c9c59"),
+		GitSha("5463cfb060336eb1c6328e6ac44cf4a68779e365"),
+		"Carlos García-Mauriño Dueñas <garcia-maurino@univention.de> 1737124337 +0100",
+		"Carlos García-Mauriño Dueñas <garcia-maurino@univention.de> 1737124337 +0100",
+		"Merge branch 'cgarcia/load-tests' into 'main'\n\ntest: load tests\n\nSee merge request univention/customers/dataport/upx/directory-importer!5",
+	}
+
+	commit, err := testRepo().Commit("6051d4147870c34253b733e6cc668055247ddb95")
+	testutils.Ok(t, err)
+
+	testutils.Equals(t, &expected, commit)
+}
